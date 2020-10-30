2d2dca982a2cb40299f9868f6f811ba68e40e704
Marianna <mana998@azet.sk>
2020-10-30 22:22:26 +0100

    Added GetMapping /teams

--- src/main/java/server/sport/controller/TeamController.java
+++ src/main/java/server/sport/controller/TeamController.java
@@ -1,21 +1,35 @@
 package server.sport.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import server.sport.model.Sport;
+import server.sport.model.Team;
+import server.sport.model.User;
+import server.sport.repository.SportRepository;
+import server.sport.repository.TeamRepository;
+import server.sport.repository.UserRepository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
 
-    @GetMapping
-    public ResponseEntity<List<Team>> getAllTutorials(@RequestParam(required = false) String title) {
+    @Autowired
+    TeamRepository teamRepository;
 
+    @GetMapping //list of teams with sport
+    public ResponseEntity<List<Team>> getAllTeams() {
+        List<Team> teams = teamRepository.findTeamsWithoutUsers();
+        if (teams.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(teams, HttpStatus.OK);
+        }
     }
+
 }
--- src/main/java/server/sport/repository/TeamRepository.java
+++ src/main/java/server/sport/repository/TeamRepository.java
@@ -1,7 +1,14 @@
 package server.sport.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import server.sport.model.Team;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+    @Query(value = "SELECT *  FROM dbo.teams t JOIN dbo.sports s ON t.sports_id = s.sports_id", nativeQuery = true)
+    List<Team> findTeamsWithoutUsers();
+
 }
