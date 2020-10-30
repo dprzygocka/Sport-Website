55b8996733d80ba9943efcc5ef7bd43413dfb5b3
Marianna <mana998@azet.sk>
2020-10-30 22:33:11 +0100

    Added PutMapping /teams/team_id - update team

--- src/main/java/server/sport/controller/TeamController.java
+++ src/main/java/server/sport/controller/TeamController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import server.sport.model.Sport;
 import server.sport.model.Team;
+import server.sport.repository.SportRepository;
 import server.sport.repository.TeamRepository;
+import server.sport.repository.UserRepository;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teams")
@@ -16,6 +20,9 @@ public class TeamController {
 
     @Autowired
     TeamRepository teamRepository;
+
+    @Autowired
+    SportRepository sportRepository;
 
     @GetMapping //list of teams with sport
     public ResponseEntity<List<Team>> getAllTeams() {
@@ -34,6 +41,21 @@ public class TeamController {
         return new ResponseEntity<>(team, HttpStatus.OK);
     }
 
+    @PutMapping("/{team_id}")//update team @RequestBody team
+    public ResponseEntity<Team> updateTeam(@PathVariable("team_id") int teamId, @RequestBody Team team) {
+        Team _team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new NoSuchElementException("Not found team with id = " + teamId));
+        _team.setTeamId(teamId);
+        _team.setTeamName(team.getTeamName());
+        Optional<Sport> optionalSport = sportRepository.findById(team.getSport().getSportsId());
+        if(optionalSport.isEmpty()){
+            Sport sport = sportRepository.save(team.getSport());
+            _team.setSport(sport);
+        } else {
+            _team.setSport(team.getSport());
+        }
+        return new ResponseEntity<>(teamRepository.save(_team), HttpStatus.OK);
+    }
 
 }
 
