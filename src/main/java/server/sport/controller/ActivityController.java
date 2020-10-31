e537b3d889bf884d2d79376eb73e3eb66b52c33d
Dagmara <przygocka.dagmara@gmail.com>
2020-10-31 17:04:07 +0100

    Added API, changes in team and activity classes

--- src/main/java/server/sport/controller/ActivityController.java
+++ src/main/java/server/sport/controller/ActivityController.java
@@ -7,7 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.sport.model.*;
 import server.sport.repository.*;
-
+import server.sport.model.Activity;
+import server.sport.model.Responsibility;
+import server.sport.model.UserResponsibility;
+import server.sport.repository.ActivityRepository;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -70,6 +74,29 @@ public class ActivityController {
         return new ResponseEntity<>(activity, HttpStatus.OK);
     }
 
+    @GetMapping("/teamActivities/{team_id}")
+    public ResponseEntity<List<Activity>> getActivitiesForTeam(@PathVariable("team_id") int teamId){
 
+        List<Activity> activities = activityRepository.findByTeamTeamId(teamId);
+
+        if (activities.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(activities, HttpStatus.OK);
+    }
+
+    @PostMapping("/{activity_id}")
+    public ResponseEntity<Activity> updateActivityInformation (@PathVariable("activity_id") int activityId, @RequestBody(required = false) Responsibility responsibility){
+        Activity activity = activityRepository.findByActivityId(activityId);
+
+        if (activity.equals(null))
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        UserResponsibility userResponsibility = new UserResponsibility(activityId, responsibility, null, activity);
+        //should I save new User resposibility ti db? or it it saved along with the list of userResposibilities
+        activity.getUserResponsibilities().add(userResponsibility);
+        activityRepository.save(activity);
+        return (new ResponseEntity<>(activity, HttpStatus.CREATED));
+    }
 
 }
--- src/main/java/server/sport/repository/ActivityRepository.java
+++ src/main/java/server/sport/repository/ActivityRepository.java
@@ -3,8 +3,10 @@ package server.sport.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.sport.model.Activity;
 
-public interface ActivityRepository extends JpaRepository <Activity, Integer> {
 
+import java.util.List;
 
-
+public interface ActivityRepository extends JpaRepository<Activity, Integer> {
+    Activity findByActivityId(int activityId);
+    List<Activity> findByTeamTeamId(int teamId);
 }
