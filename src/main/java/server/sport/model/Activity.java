fbf6d543309bd0f29246b531706296d889b21536
Dagmara <przygocka.dagmara@gmail.com>
2020-10-31 17:04:07 +0100

    Added API, changes in team and activity classes

--- src/main/java/server/sport/model/Activity.java
+++ src/main/java/server/sport/model/Activity.java
@@ -18,6 +18,7 @@ public class Activity {
     private Collection<ActivityStatus> activityStatuses;
     private Match match;
     private Collection<UserResponsibility> userResponsibilities;
+    private Team team;
 
     @Id
     @Column(name = "activity_id", nullable = false)
@@ -115,6 +116,16 @@ public class Activity {
 
     public void setActivityType(ActivityType activityType) {
         this.activityType = activityType;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", referencedColumnName = "team_id", nullable = false)
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
     @OneToOne
--- src/main/java/server/sport/repository/ActivityRepository.java
+++ src/main/java/server/sport/repository/ActivityRepository.java
@@ -1,4 +1,11 @@
 package server.sport.repository;
 
-public interface ActivityRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import server.sport.model.Activity;
+
+import java.util.List;
+
+public interface ActivityRepository extends JpaRepository<Activity, Integer> {
+    Activity findByActivityId(int activityId);
+    List<Activity> findByTeamTeamId(int teamId);
 }
