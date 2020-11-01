ce9e1a95a9a5afda6709560e17e189ad51ee3092
Nesrin Saxeide Kristensen <nesrin.tufan@gmail.com>
2020-11-01 23:45:54 +0100

    create activity method

--- src/main/java/server/sport/controller/ActivityController.java
+++ src/main/java/server/sport/controller/ActivityController.java
@@ -1,10 +1,24 @@
 package server.sport.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< HEAD
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+=======
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+>>>>>>> create activity method
 import server.sport.model.*;
 import server.sport.repository.*;
 import server.sport.model.Activity;
@@ -15,6 +29,10 @@ import java.util.List;
 import java.util.Optional;
 
 
+<<<<<<< HEAD
+=======
+@RequestMapping("/activities")
+>>>>>>> create activity method
 @RestController
 @RequestMapping("/activities")
 public class ActivityController {
@@ -34,8 +52,98 @@ public class ActivityController {
     @Autowired
     MatchRepository matchRepository;
 
+<<<<<<< HEAD
 
     @PostMapping("/activity")
+=======
+    private Sort.Direction getSortDirection (String direction){
+
+        if (direction.equals("asc")){
+            return Sort.Direction.ASC;
+        }
+        return Sort.Direction.DESC;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String, Object>> getPageOfActivities(
+            @RequestParam(defaultValue = "0")int page, //activities to be loaded on page 0
+            @RequestParam(defaultValue = "3")int size, //3 activities will be fetched from the database
+            @RequestParam(defaultValue = "id,desc")String[] sort //ordered by descending order
+    ){
+
+        List<Order> orders = new ArrayList<>();
+        if (sort[0].contains(",")){
+            for (String sortOrder : sort){
+                String[] _sort = sortOrder.split(",");
+                orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
+            }
+
+        }else{
+            orders.add(new Order(getSortDirection(sort[1]), sort[0]));
+        }
+
+        Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders)); //create Pageable object with page, size, and sort parameters
+
+        //establishes what is on the pageOfActivities.
+        Page<Activity> pageOfActivities;
+        pageOfActivities = activityRepository.findAll(pagingSort); //includes information relating to the page itself such as size, page number
+        List<Activity> activities = pageOfActivities.getContent(); //retrieves List of items in the page
+
+        if (activities.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("activities", activities);
+        response.put("currentPage", pageOfActivities.getNumber());
+        response.put("totalActivities", pageOfActivities.getTotalElements());
+        response.put("totalPages", pageOfActivities.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{activity_id}")
+    public ResponseEntity<Optional<Activity>> getActivityById(@PathVariable("activity_id") int activityId){
+
+        Optional<Activity> activity = activityRepository.findById(activityId);
+        System.out.println(activity.toString());
+
+        if (activity.equals(null)){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(activity, HttpStatus.OK);
+    }
+
+    @GetMapping("/teamActivities/{team_id}")
+    public ResponseEntity<List<Activity>> getActivitiesForTeam(@PathVariable("team_id") int teamId){
+
+        List<Activity> activities = activityRepository.findByTeamTeamId(teamId);
+
+        if (activities.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(activities, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/{activity_id}")
+    public ResponseEntity<Activity> updateActivityInformation (@PathVariable("activity_id") int activityId, @RequestBody(required = false) Responsibility responsibility){
+        Activity activity = activityRepository.findByActivityId(activityId);
+
+        if (activity.equals(null))
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        UserResponsibility userResponsibility = new UserResponsibility(activityId, responsibility, null, activity);
+        //should I save new User resposibility ti db? or it it saved along with the list of userResposibilities
+        activity.getUserResponsibilities().add(userResponsibility);
+        activityRepository.save(activity);
+        return (new ResponseEntity<>(activity, HttpStatus.CREATED));
+    }
+
+
+    @PostMapping("/")
+>>>>>>> create activity method
     public ResponseEntity<Activity> createActivity (@RequestBody Activity activity){
 
         Location location = locationRepository.findLocationByCourtName(activity.getReservation().getLocation().getCourtName())
@@ -62,6 +170,7 @@ public class ActivityController {
 
     }
 
+<<<<<<< HEAD
     @GetMapping("/{activity_id}")
     public ResponseEntity<Optional<Activity>> getActivityById(@PathVariable("activity_id") int activityId){
 
@@ -98,5 +207,10 @@ public class ActivityController {
         activityRepository.save(activity);
         return (new ResponseEntity<>(activity, HttpStatus.CREATED));
     }
+=======
+
+
+
+>>>>>>> create activity method
 
 }
--- src/main/java/server/sport/repository/ActivityRepository.java
+++ src/main/java/server/sport/repository/ActivityRepository.java
@@ -1,12 +1,26 @@
 package server.sport.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.sport.model.Activity;
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> create activity method
 import java.util.List;
+
 
 public interface ActivityRepository extends JpaRepository<Activity, Integer> {
     Activity findByActivityId(int activityId);
     List<Activity> findByTeamTeamId(int teamId);
+<<<<<<< HEAD
+=======
+    Page<Activity> findByActivityName(String activityName, Pageable paging);
+    Page<Activity> findByCapacity(int capacity, Pageable paging);
+    Page<Activity> findByActivityType(String activityType, Pageable paging);
+
+
+>>>>>>> create activity method
 }
