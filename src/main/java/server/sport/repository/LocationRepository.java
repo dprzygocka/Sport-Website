a74ad7ebb593c17f8b0bdfc4bc5f52efc5972533
Nesrin Saxeide Kristensen <nesrin.tufan@gmail.com>
2020-11-03 17:16:20 +0100

    unfinished testdelete methods

--- src/main/java/server/sport/repository/LocationRepository.java
+++ src/main/java/server/sport/repository/LocationRepository.java
@@ -1,12 +1,15 @@
 package server.sport.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.sport.model.Location;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
     List<Location> findAllByReservationsDateTime(Date date);
+    List<Location> findByCourtName(String courtName);
+    List<Location> deleteById (int locationId);
+
 }
--- src/test/java/server/sport/controller/LocationControllerTest.java
+++ src/test/java/server/sport/controller/LocationControllerTest.java
@@ -1,20 +1,34 @@
 package server.sport.controller;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import server.sport.controller.LocationController;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 import server.sport.model.Location;
 import server.sport.repository.LocationRepository;
 import server.sport.repository.SportRepository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class LocationControllerTest {
     @Autowired
     LocationController locationController;
@@ -23,10 +37,11 @@ public class LocationControllerTest {
     @Autowired
     SportRepository sportRepository;
 
+
     //--- Create Location Test ---//
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
     @Test
@@ -56,4 +71,27 @@ public class LocationControllerTest {
 
         assertThat(locationResponseEntity.getBody().getCourtName().equals(newLocationName));
     }
+
+
+    //I'm aware that this doesn't test controller. I just need to test interact with the H2 database first
+    @Test
+    @Rollback(false)
+    public void whenDeletingLocationFromRepoItShouldBeSuccessful(){
+        locationRepository.deleteById(6); //does not find id 6
+        List<Location> deletedLocation = locationRepository.findByCourtName("Basketball Pitch 2");
+        assertThat(deletedLocation).isNull();
+    }
+
+    @Test
+    @Rollback(false)
+    public void testLocationIsDeleted(){
+        Integer id = 6;
+        boolean existingBeforeDelete = locationRepository.findById(id).isPresent();
+        locationRepository.deleteById(id);
+        boolean notExist= locationRepository.findById(id).isPresent();
+        assertTrue(existingBeforeDelete);
+        assertFalse(notExist);
+    }
+
+
 }
