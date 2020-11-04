c18ef4947d75fd3f18a72c2ad6d6bfabe9feaed0
Nesrin Saxeide Kristensen <nesrin.tufan@gmail.com>
2020-11-04 18:39:28 +0100

    Just made a new empty class

--- src/test/java/server/sport/controller/ReservationControllerTest.java
+++ src/test/java/server/sport/controller/ReservationControllerTest.java
@@ -1,2 +1,26 @@
-package server.sport.controller;public class ReservationControllerTest {
+package server.sport.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReservationControllerTest {
+
+    @Autowired //need to rebase to get this
+    ReservationController reservationController;
+
+    @Autowired // need to rebase to get this
+    ReservationRepository reservationRepository;
+
+
+
+
+
+
+
 }
