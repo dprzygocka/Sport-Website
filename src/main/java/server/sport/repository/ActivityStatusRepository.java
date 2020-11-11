4d5913b51b7b65ce2c351b75e16be90d99053ba7
Dagmara <przygocka.dagmara@gmail.com>
2020-11-11 16:08:58 +0100

    Revert "Merge branch 'Put-status-profiles' into teams-api-fix"
    This reverts commit 50a57988dac5bb8e7facc8356a616a2d4dc0a4f4.

--- src/main/java/server/sport/repository/ActivityStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package server.sport.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import server.sport.model.ActivityStatus;
-import server.sport.model.ActivityStatusPK;
-
-public interface ActivityStatusRepository extends JpaRepository<ActivityStatus, ActivityStatusPK> {
-}
