3d8fe8c07b8b5ea0d31b37a1feea07bea93c1158
Marianna <mana998@azet.sk>
2020-11-15 08:45:46 +0100

    Replaced findTeamsWithoutUsers by findAll

--- src/main/java/server/sport/repository/TeamRepository.java
+++ src/main/java/server/sport/repository/TeamRepository.java
@@ -9,9 +9,9 @@ import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Integer> {
 
-    //it takes user either way...
+    /*//it takes user either way...
     @Query(value = "SELECT t.team_id,t.team_name, t.sport_id, s.sport_name  FROM dbo.teams t JOIN dbo.sports s ON t.sport_id = s.sport_id", nativeQuery = true)
-    List<Team> findTeamsWithoutUsers();
+    List<Team> findTeamsWithoutUsers();*/
 
     Optional<Team> findTeamByTeamName(String teamName);
 }
